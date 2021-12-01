57d19b662e301c896461d4f05eba339a0ca97603
Gabriel Ramos <dev.gabrielramos@gmail.com>
2021-12-01 16:53:03 -0300

    Remove unused components

--- src/components/Input/index.tsx
+++
@@ -1,7 +0,0 @@
-import { InputHTMLAttributes } from 'react'
-
-import { Container } from './styles'
-
-export const Input = (props: InputHTMLAttributes<HTMLInputElement>) => {
-  return <Container {...props} />
-}
