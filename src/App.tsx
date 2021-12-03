d6702437480b4826489a4466071aca0069f51171
Gabriel Ramos <dev.gabrielramos@gmail.com>
2021-12-03 00:11:08 -0300

    Update and simulate private routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
-import { ContentRoutes } from 'components/ContentRoutes'
+import { Routes, Route } from 'react-router-dom'
+
+import { UserAuthPage } from 'pages/UserAuthPage'
+import { Home } from 'pages/Home'
+import { Dashboard } from 'pages/Dashboard'
+
 import { Footer } from 'components/Footer'
 
 export const App = () => {
   return (
     <>
-      <ContentRoutes />
+      <Routes>
+        <Route path='/*' element={<UserAuthPage />} />
+        <Route
+          path='/home/*'
+          element={true ? <Home /> : <h1>O usuário não está autenticado!</h1>}
+        />
+        <Route
+          path='/dashboard'
+          element={
+            true ? <Dashboard /> : <h1>O usuário não está autenticado!</h1>
+          }
+        />
+      </Routes>
       <Footer />
     </>
   )
