0c7ec198ec0daccd58acb9c4a1b865f6c25cef62
Gabriel Ramos <dev.gabrielramos@gmail.com>
2021-12-07 11:42:25 -0300

    Add features in the NewBet component

--- src/pages/Dashboard/NewBet/index.tsx
+++ src/pages/Dashboard/NewBet/index.tsx
@@ -1,6 +1,8 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, MouseEvent } from 'react'
 import { AiOutlineShoppingCart } from 'react-icons/ai'
 import { useSelector } from 'react-redux'
+import toast, { Toaster } from 'react-hot-toast'
+import { useNavigate } from 'react-router-dom'
 
 import { IAvailableGame } from 'store/modules/availableGames/types'
 import { RootState } from 'store/modules/rootReducer'
@@ -9,7 +11,8 @@ import * as S from './styles'
 export const NewBet = () => {
   const [isLoadingAvailableGames, setIsLoadingAvailableGames] = useState(true)
   const [selectedGame, setSelectedGame] = useState<IAvailableGame | null>(null)
-
+  const [choosenGameNumbers, setChoosenGameNumbers] = useState<number[]>([])
+  const navigate = useNavigate()
   const availableGames = useSelector((state: RootState) => state.availableGames)
 
   useEffect(() => {
@@ -21,15 +24,86 @@ export const NewBet = () => {
         setSelectedGame(defaultGame)
       }, 500)
     }
-  }, [availableGames])
+  }, [availableGames, navigate])
+
+  const getGameNumbers = (range = 0): number[] => {
+    const gameNumbers: number[] = []
+
+    for (let gameNumber = 0; gameNumbers.length <= range; gameNumber++) {
+      gameNumbers.push(gameNumber)
+    }
+
+    return gameNumbers
+  }
+
+  const handleClickGameNumberButton = (
+    event: MouseEvent<HTMLButtonElement>,
+  ) => {
+    const gameNumberButton = event.target as HTMLButtonElement
+    const newChoosenGameNumber = Number(gameNumberButton.value)
+    const hasTheNumberAlreadyBeenChosen =
+      choosenGameNumbers.includes(newChoosenGameNumber)
+    const maxNumbersAllowed = selectedGame?.max_number || 0
+    const hasSpaceForOneMoreNumber =
+      choosenGameNumbers.length < maxNumbersAllowed
+
+    if (hasTheNumberAlreadyBeenChosen) {
+      setChoosenGameNumbers((prevChoosenGameNumbers) =>
+        prevChoosenGameNumbers.filter(
+          (choosenGameNumber) => choosenGameNumber !== newChoosenGameNumber,
+        ),
+      )
+
+      toast.remove()
+      toast.error('Número removido.')
+      return
+    }
+
+    if (hasSpaceForOneMoreNumber) {
+      setChoosenGameNumbers((prevChoosenGameNumbers) =>
+        prevChoosenGameNumbers.concat(newChoosenGameNumber),
+      )
+
+      toast.remove()
+      toast.success('Número selecionado.')
+      return
+    }
+
+    toast.remove()
+    toast.error('Você atingiu a quantidade máxima de números selecionados.')
+  }
+
+  const getRandomNumbers = (maxNumber = 0, range = 0): number[] => {
+    const randomNumbers: number[] = []
+
+    do {
+      const randomNumber = Math.round(Math.random() * range)
+
+      if (!randomNumbers.includes(randomNumber)) {
+        randomNumbers.push(randomNumber)
+      }
+    } while (randomNumbers.length !== maxNumber)
+
+    return randomNumbers
+  }
+
+  const completeGame = () => {
+    const randomNumbers =
+      getRandomNumbers(selectedGame?.max_number, selectedGame?.range)
+    setChoosenGameNumbers(randomNumbers)
+  }
+
+  const clearGame = () => {
+    setChoosenGameNumbers([])
+  }
 
   if (isLoadingAvailableGames) {
     return <p>Loading...</p>
   }
-  console.log(availableGames)
 
   return (
     <section>
+      <Toaster />
       <S.Title>
         New Bet <span>for {selectedGame?.type}</span>
       </S.Title>
@@ -42,11 +116,13 @@ export const NewBet = () => {
               to={`/dashboard/${availableGame.id}`}
               theme={availableGame.color}
               onClick={() => {
-                const selectedGameById = availableGames.types.find(
-                  (availableGameType) =>
-                    availableGameType.id === availableGame.id,
-                ) || null
+                const selectedGameById =
+                  availableGames.types.find(
+                    (availableGameType) =>
+                      availableGameType.id === availableGame.id,
+                  ) || null
 
+                setChoosenGameNumbers([])
                 setSelectedGame(selectedGameById)
               }}
             >
@@ -58,25 +134,22 @@ export const NewBet = () => {
       <S.Subtitle>Fill your bet</S.Subtitle>
       <S.GameDescription>{selectedGame?.description}</S.GameDescription>
       <S.GameOptionsContainer>
-        <S.GameNumber />
-        <S.GameNumber />
-        <S.GameNumber />
-        <S.GameNumber />
-        <S.GameNumber />
-        <S.GameNumber />
-        <S.GameNumber />
-        <S.GameNumber />
-        <S.GameNumber />
-        <S.GameNumber />
-        <S.GameNumber />
-        <S.GameNumber />
-        <S.GameNumber />
-        <S.GameNumber />
+        {getGameNumbers(selectedGame?.range).map((gameNumber) => (
+          <S.GameNumber
+            key={gameNumber}
+            value={gameNumber}
+            onClick={handleClickGameNumberButton}
+            isActive={choosenGameNumbers.includes(gameNumber)}
+            buttonColor={selectedGame?.color || '#fff'}
+          >
+            {gameNumber}
+          </S.GameNumber>
+        ))}
       </S.GameOptionsContainer>
       <S.GameActionsContainer>
         <S.GameActionsContainerInner>
-          <S.Button>Complete Game</S.Button>
-          <S.Button>Clear Game</S.Button>
+          <S.Button onClick={completeGame}>Complete Game</S.Button>
+          <S.Button onClick={clearGame}>Clear Game</S.Button>
         </S.GameActionsContainerInner>
         <S.Button isFill>
           <AiOutlineShoppingCart />
