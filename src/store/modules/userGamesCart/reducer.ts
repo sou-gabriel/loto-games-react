e9d87170ef683da76b95ff383159575d599f94df
Gabriel Ramos <dev.gabrielramos@gmail.com>
2021-12-10 16:46:09 -0300

    feat: Sort the cart numbers

--- src/store/modules/userGamesCart/reducer.ts
+++ src/store/modules/userGamesCart/reducer.ts
@@ -3,9 +3,14 @@ import { IAction, IGame } from './types'
 export const userGamesCartReducer = (state: IGame[] = [], action: IAction) => {
   switch (action.type) {
     case 'ADD_GAME_TO_CART':
-      return [...state, action.payload.game]
+      const newGame = action.payload.game
+      const newGameNumbersSorted = newGame?.numbers
+        .map((number) => number)
+        .sort((b, a) => b - a)
+
+      return [...state, { ...newGame, numbers: newGameNumbersSorted }]
     case 'REMOVE_GAME_FROM_CART':
-      return state.filter(game => game.id !== action.payload.id)
+      return state.filter((game) => game.id !== action.payload.id)
     case 'REMOVE_ALL_GAMES':
       return []
     default:
